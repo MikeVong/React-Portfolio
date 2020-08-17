474c68065a353e0cc14e34077fd0f9a2b6cb3a0e
Mike Vong <mv910@hotmail.com>
2020-08-16 21:58:06 -0700

    Updates

---
+++ precache-manifest.eeacf09614f35ac57ace95ba5dd39df3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e896cf1776bd3ffb0151378fa83bb498",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "92adcbb55652dd509ecb",
+    "url": "/React-Portfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "750c559896205dac3b25",
+    "url": "/React-Portfolio/static/css/main.1bdaf2da.chunk.css"
+  },
+  {
+    "revision": "92adcbb55652dd509ecb",
+    "url": "/React-Portfolio/static/js/2.602e2ce6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.602e2ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750c559896205dac3b25",
+    "url": "/React-Portfolio/static/js/main.4fa467f7.chunk.js"
+  },
+  {
+    "revision": "2dc36cc20cfe214543fc",
+    "url": "/React-Portfolio/static/js/runtime-main.6d8b9d52.js"
+  },
+  {
+    "revision": "8b2a5e34d51814b44abb2200cf39a99c",
+    "url": "/React-Portfolio/static/media/dynamic.8b2a5e34.png"
+  },
+  {
+    "revision": "12b18591b6c0afc40f476c690555e1af",
+    "url": "/React-Portfolio/static/media/emailSquare.12b18591.png"
+  },
+  {
+    "revision": "d1f4917bae4b2282f0e8b48ca0b98d63",
+    "url": "/React-Portfolio/static/media/fast.d1f4917b.jpg"
+  },
+  {
+    "revision": "d36205d230eefbcfcccca14ef6853b8e",
+    "url": "/React-Portfolio/static/media/gamer.d36205d2.gif"
+  },
+  {
+    "revision": "2dcddfeb1526256c6a1250fd18e03343",
+    "url": "/React-Portfolio/static/media/google.2dcddfeb.gif"
+  },
+  {
+    "revision": "a191d8ddd654e97dd4d6e33d0c17be28",
+    "url": "/React-Portfolio/static/media/google.a191d8dd.jpg"
+  },
+  {
+    "revision": "cd0129c9e35a350b02e463def601c86d",
+    "url": "/React-Portfolio/static/media/homecook.cd0129c9.jpg"
+  },
+  {
+    "revision": "f2c7f36fc1d4baca99d199582351a014",
+    "url": "/React-Portfolio/static/media/homecook.f2c7f36f.gif"
+  },
+  {
+    "revision": "2866a6139048ea754d6f8a8785cfbe09",
+    "url": "/React-Portfolio/static/media/job.2866a613.gif"
+  },
+  {
+    "revision": "e47055e9691bd4bf01fc23552a5ec874",
+    "url": "/React-Portfolio/static/media/job.e47055e9.jpg"
+  },
+  {
+    "revision": "d0d9090a3c48b2f3b724c5bd46137656",
+    "url": "/React-Portfolio/static/media/linkedinSquare.d0d9090a.png"
+  },
+  {
+    "revision": "b68c0733b89b9921f48795746cd877d6",
+    "url": "/React-Portfolio/static/media/me.b68c0733.jpg"
+  },
+  {
+    "revision": "49ea64f2a61747bbf8f6f9d844a73742",
+    "url": "/React-Portfolio/static/media/mongodb.49ea64f2.jpg"
+  },
+  {
+    "revision": "c2bf31dd35a9a943804fe2cbda638f73",
+    "url": "/React-Portfolio/static/media/newscraper.c2bf31dd.gif"
+  },
+  {
+    "revision": "569b1f0ac5e40c85f4ec7796edd56b17",
+    "url": "/React-Portfolio/static/media/project1.569b1f0a.jpg"
+  },
+  {
+    "revision": "bada42de09b27276496e46dabd4b0af5",
+    "url": "/React-Portfolio/static/media/train.bada42de.jpg"
+  },
+  {
+    "revision": "f540b5ea4a31c3f3495b709b35c451de",
+    "url": "/React-Portfolio/static/media/train.f540b5ea.gif"
+  }
+]);
